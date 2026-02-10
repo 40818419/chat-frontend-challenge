c6ba80875a9a42206bdcc33b95c2bcb79d87e35c
Konstantin Kulinicenko <konstantin.kulinicenko@gmail.com>
2026-02-10 22:00:48 +0100

    fix: prevent refetch on window focus

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,13 @@
 'use client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export default function Providers({ children }: {children: React.ReactNode}) {
   return (
